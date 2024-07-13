1c8cb8403c272bb1013e22b9e0831e7d70c99306
John Parsaie <parsaiej@gmail.com>
2024-07-13 13:10:58 -0400

    Create Vulkan Device.

--- Source/Main.cpp
+++ Source/Main.cpp
@@ -1,8 +1,15 @@
+#include <climits>
+#include <cstdint>
 #include <dxgi.h>
 #include <d3d11.h>
-#include <locale>
+
+#include <volk.h>
+#include <vulkan/vulkan_core.h>
+
+#define VMA_IMPLEMENTATION
+#include <vk_mem_alloc.h>
+
 #include <spdlog/spdlog.h>
-#include <thread>
 
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include <stb_image_write.h>
@@ -14,11 +21,85 @@
 
 #include <format>
 #include <wrl.h>
+#include <codecvt>
 
 using namespace Microsoft::WRL;
 
 const UINT kTestImageWidth  = 1920;
 const UINT kTestImageHeight = 1080;
+
+bool SelectVulkanPhysicalDevice(VkInstance vkInstance, IDXGIAdapter* pDXGIAdapter, VkPhysicalDevice& vkPhysicalDevice)
+{    
+    uint32_t deviceCount = 0u;
+    vkEnumeratePhysicalDevices(vkInstance, &deviceCount, nullptr);
+
+    std::vector<VkPhysicalDevice> vkPhysicalDevices(deviceCount);
+    vkEnumeratePhysicalDevices(vkInstance, &deviceCount, vkPhysicalDevices.data());
+
+    DXGI_ADAPTER_DESC selectedAdapterDesc;
+    pDXGIAdapter->GetDesc(&selectedAdapterDesc);
+
+    std::wstring_convert<std::codecvt_utf8<wchar_t>, wchar_t> wideStringConvert;
+    const auto dxgiAdapterName = wideStringConvert.to_bytes(selectedAdapterDesc.Description);
+
+    vkPhysicalDevice = VK_NULL_HANDLE;
+
+    for (const auto& physicalDevice : vkPhysicalDevices)
+    {
+        VkPhysicalDeviceProperties physicalDeviceProperties;
+        vkGetPhysicalDeviceProperties(physicalDevice, &physicalDeviceProperties);
+
+        if (strcmp(dxgiAdapterName.c_str(), physicalDeviceProperties.deviceName))
+            continue;
+
+        // Found the matching Vulkan Physical Device for the existing DXGI Adapter.
+        vkPhysicalDevice = physicalDevice;
+
+        break;
+    }
+
+    return vkPhysicalDevice != VK_NULL_HANDLE;
+}
+
+bool GetVulkanGraphicsQueueIndexFromDevice(VkPhysicalDevice vkPhysicalDevice, uint32_t& graphicsQueueIndex)
+{
+    graphicsQueueIndex = UINT_MAX;
+
+    uint32_t queueFamilyCount = 0u;
+    vkGetPhysicalDeviceQueueFamilyProperties(vkPhysicalDevice, &queueFamilyCount, nullptr);
+
+    std::vector<VkQueueFamilyProperties> queueFamilyProperties(queueFamilyCount);
+    vkGetPhysicalDeviceQueueFamilyProperties(vkPhysicalDevice, &queueFamilyCount, queueFamilyProperties.data());
+
+    for (uint32_t queueFamilyIndex = 0; queueFamilyIndex < queueFamilyCount; queueFamilyIndex++)
+    {
+        if (!(queueFamilyProperties[queueFamilyIndex].queueFlags & VK_QUEUE_GRAPHICS_BIT))
+            continue;
+
+        // Choose the first graphics queue we find.
+        graphicsQueueIndex = queueFamilyIndex;
+
+        break;
+    }
+
+    return graphicsQueueIndex != UINT_MAX;
+}
+
+bool CreateVulkanLogicalDevice(VkPhysicalDevice vkPhysicalDevice, uint32_t vkGraphicsQueueIndex, VkDevice& vkLogicalDevice)
+{
+    float graphicsQueuePriority = 1.0;
+
+    VkDeviceQueueCreateInfo vkGraphicsQueueCreateInfo = { VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO };
+    vkGraphicsQueueCreateInfo.queueFamilyIndex = vkGraphicsQueueIndex;
+    vkGraphicsQueueCreateInfo.queueCount       = 1u;
+    vkGraphicsQueueCreateInfo.pQueuePriorities = &graphicsQueuePriority;
+
+    VkDeviceCreateInfo vkLogicalDeviceCreateInfo = { VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO };
+    vkLogicalDeviceCreateInfo.pQueueCreateInfos    = &vkGraphicsQueueCreateInfo;
+    vkLogicalDeviceCreateInfo.queueCreateInfoCount = 1u;
+
+    return vkCreateDevice(vkPhysicalDevice, &vkLogicalDeviceCreateInfo, nullptr, &vkLogicalDevice) == VK_SUCCESS;
+}
 
 bool SelectDXGIAdapter(IDXGIAdapter** ppSelectedAdapter)
 {
@@ -106,6 +187,78 @@ int main()
     if (pRenderDocAPI) pRenderDocAPI->StartFrameCapture(pDeviceDX.Get(), nullptr);
 #endif
 
+    // Initialize Vulkan
+    // ------------------------------------------------
+
+    if (volkInitialize() != VK_SUCCESS)
+    {
+        spdlog::critical("Failed to initialize Volk.");
+        return 1;
+    }
+
+    VkApplicationInfo vkApplicationInfo = { VK_STRUCTURE_TYPE_APPLICATION_INFO };
+    vkApplicationInfo.pApplicationName   = "SharedMemory-Vulkan-D3D11";
+    vkApplicationInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+    vkApplicationInfo.pEngineName        = "No Engine";
+    vkApplicationInfo.engineVersion      = VK_MAKE_VERSION(0, 0, 0);
+    vkApplicationInfo.apiVersion         = VK_API_VERSION_1_3;
+
+    VkInstanceCreateInfo vkInstanceCreateInfo = { VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO };
+    vkInstanceCreateInfo.pApplicationInfo = &vkApplicationInfo;
+
+    VkInstance vkInstance;
+    if (vkCreateInstance(&vkInstanceCreateInfo, nullptr, &vkInstance) != VK_SUCCESS)
+    {
+        spdlog::critical("Failed to create the Vulkan Instance.");
+        return 1;
+    }
+
+    volkLoadInstanceOnly(vkInstance);
+
+    VkPhysicalDevice vkPhysicalDevice;
+    if (!SelectVulkanPhysicalDevice(vkInstance, pAdapter.Get(), vkPhysicalDevice))
+    {
+        spdlog::critical("Failed to select a Vulkan Physical Device.");
+        return 1;
+    }
+
+    uint32_t vkGraphicsQueueIndex;
+    if (!GetVulkanGraphicsQueueIndexFromDevice(vkPhysicalDevice, vkGraphicsQueueIndex))
+    {
+        spdlog::critical("Failed to get the graphics queue from the selected Vulkan Physical Device.");
+        return 1;
+    }
+
+    VkDevice vkLogicalDevice;
+    if (!CreateVulkanLogicalDevice(vkPhysicalDevice, vkGraphicsQueueIndex, vkLogicalDevice))
+    {
+        spdlog::critical("Failed to create the Vulkan Logical Device");
+        return 1;
+    }
+
+    volkLoadDevice(vkLogicalDevice);
+
+    VmaAllocator vkMemoryAllocator;
+    {
+        VmaVulkanFunctions vkMemoryAllocatorFunctions = {};
+        vkMemoryAllocatorFunctions.vkGetInstanceProcAddr = vkGetInstanceProcAddr;
+        vkMemoryAllocatorFunctions.vkGetDeviceProcAddr   = vkGetDeviceProcAddr;
+        
+        VmaAllocatorCreateInfo vkMemoryAllocatorCreateInfo = {};
+        vkMemoryAllocatorCreateInfo.flags            = VMA_ALLOCATOR_CREATE_EXT_MEMORY_BUDGET_BIT;
+        vkMemoryAllocatorCreateInfo.vulkanApiVersion = VK_API_VERSION_1_2;
+        vkMemoryAllocatorCreateInfo.physicalDevice   = vkPhysicalDevice;
+        vkMemoryAllocatorCreateInfo.device           = vkLogicalDevice;
+        vkMemoryAllocatorCreateInfo.instance         = vkInstance;
+        vkMemoryAllocatorCreateInfo.pVulkanFunctions = &vkMemoryAllocatorFunctions;
+        
+        if (vmaCreateAllocator(&vkMemoryAllocatorCreateInfo, &vkMemoryAllocator) != VK_SUCCESS)
+        {
+            spdlog::critical("Failed to create the Vulkan Memory Allocator.");
+            return 1;
+        }
+    }
+
     // Create GPU-native Image Resource.
     // ------------------------------------------------
 
@@ -118,6 +271,7 @@ int main()
     imageDesc.Usage     = D3D11_USAGE_DEFAULT;
     imageDesc.BindFlags = D3D11_BIND_RENDER_TARGET | D3D11_BIND_SHADER_RESOURCE;
     imageDesc.SampleDesc.Count = 1;
+    imageDesc.MiscFlags = D3D11_RESOURCE_MISC_SHARED;
 
     ComPtr<ID3D11Texture2D> pImageDX;
     if (!SUCCEEDED(pDeviceDX->CreateTexture2D(&imageDesc, nullptr, pImageDX.GetAddressOf())))
@@ -126,6 +280,19 @@ int main()
         return 1;
     }
 
+    // Open a shareable handle to D3D11 Image Resource.
+    // ------------------------------------------------
+
+    ComPtr<IDXGIResource> pSharedResource;
+    if (!SUCCEEDED(pImageDX->QueryInterface(IID_PPV_ARGS(pSharedResource.GetAddressOf()))))
+    {
+        spdlog::critical("Failed.");
+        return 1;
+    }
+
+    HANDLE sharedHandle;
+    pSharedResource->GetSharedHandle(&sharedHandle);
+
     // Create CPU-Accessible Staging Image Resource.
     // ------------------------------------------------
 
@@ -133,6 +300,7 @@ int main()
     stagingImageDesc.Usage          = D3D11_USAGE_STAGING;
     stagingImageDesc.CPUAccessFlags = D3D11_CPU_ACCESS_READ;
     stagingImageDesc.BindFlags      = 0u;
+    stagingImageDesc.MiscFlags      = 0u;
     
     ComPtr<ID3D11Texture2D> pStagingImageDX;
     if (!SUCCEEDED(pDeviceDX->CreateTexture2D(&stagingImageDesc, nullptr, pStagingImageDX.GetAddressOf())))
@@ -168,9 +336,14 @@ int main()
         spdlog::critical("Failed to map a pointer to the staging image memory.");
         return 1;
     }
-
+    
     // Write out the result to disk.
     stbi_write_jpg("Output.jpg", 1920, 1080, 4, mappedStagingMemory.pData, 100);
+
+    // Release Vulkan Primitives.
+    vmaDestroyAllocator(vkMemoryAllocator);
+    vkDestroyDevice(vkLogicalDevice, nullptr);
+    vkDestroyInstance(vkInstance, nullptr);
 
     return 0;
 }
